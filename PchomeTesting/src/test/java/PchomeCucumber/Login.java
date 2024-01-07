5c8097afcc3a66fe11c3814575e3529d0c08a11a
poyu39 <bruce920309@gmail.com>
2024-01-07 12:29:54 +0800

    feat: 新增 headless

--- PchomeTesting/src/test/java/PchomeCucumber/Login.java
+++ PchomeTesting/src/test/java/PchomeCucumber/Login.java
@@ -11,6 +11,7 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import java.time.Duration;
+import java.util.concurrent.TimeUnit;
 
 public class Login {
     WebDriver driver;
@@ -18,9 +19,10 @@ public class Login {
     public void beforeScenario() {
         ChromeOptions options = new ChromeOptions();
         options.addArguments("--remote-allow-origins=*");   // 允許遠端控制瀏覽器
-        options.addArguments("--window-size=1920,1080");    // 設定視窗大小 (寬, 高)
+        options.addArguments("--headless");                 // 不開啟瀏覽器
         driver = new ChromeDriver(options);
         driver.manage().deleteAllCookies();                 // 刪除瀏覽器所有 cookie
+        driver.manage().window().maximize();                // 最大化瀏覽器
         // 設定 2 sec 的搜尋等待時間 (for driver.findElement)
         driver.manage().timeouts().implicitlyWait(Duration.ofMillis(2000));
         driver.get("https://24h.pchome.com.tw/");           // 進入網址內容
