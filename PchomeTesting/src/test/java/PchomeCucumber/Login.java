0429c33049df023b86f762aa30fafde63a8ad017
poyu39 <bruce920309@gmail.com>
2024-01-08 14:54:23 +0800

    feat: 登入改使用手動驗證

--- PchomeTesting/src/test/java/PchomeCucumber/Login.java
+++ PchomeTesting/src/test/java/PchomeCucumber/Login.java
@@ -19,12 +19,12 @@ public class Login {
     public void beforeScenario() {
         ChromeOptions options = new ChromeOptions();
         options.addArguments("--remote-allow-origins=*");   // 允許遠端控制瀏覽器
-        options.addArguments("--headless");                 // 不開啟瀏覽器
+        // options.addArguments("--headless");                 // 不開啟瀏覽器
         driver = new ChromeDriver(options);
         driver.manage().deleteAllCookies();                 // 刪除瀏覽器所有 cookie
         driver.manage().window().maximize();                // 最大化瀏覽器
         // 設定 2 sec 的搜尋等待時間 (for driver.findElement)
-        driver.manage().timeouts().implicitlyWait(Duration.ofMillis(2000));
+        driver.manage().timeouts().implicitlyWait(Duration.ofMillis(5000));
         driver.get("https://24h.pchome.com.tw/");           // 進入網址內容
     }
 
@@ -60,19 +60,40 @@ public class Login {
 
     @Then("使用者應該登入成功")
     public void userShouldBeLoggedIn() throws InterruptedException {
+        /*
         // 檢查是否有 google reCAPTCHA
+        boolean hasGoogleReCAPTCHA = false;
         try {
-            WebElement element = driver.findElement(By.xpath("//iframe[@title='recaptcha challenge']"));
+            WebElement element = driver.findElement(By.xpath("//input[@value='authRecaptcha']"));
             element.click();
+            WebElement element2 = driver.findElement(By.xpath("//button[@id='btnChoice']"));
+            element2.click();
+            hasGoogleReCAPTCHA = true;
         } catch (Exception e) {
             System.out.println("No google reCAPTCHA");
         }
+        // 手動驗證
+        if (hasGoogleReCAPTCHA) {
+            while (true) {
+                try {
+                    WebElement element3 = driver.findElement(By.xpath("//input[@recaptcha-token!=' ']"));
+                    if (element3.isDisplayed()) break;
+                } catch (Exception e) {
+                    TimeUnit.SECONDS.sleep(1);
+                }
+            }
+            WebElement element4 = driver.findElement(By.xpath("//button[@id='btnLogin']"));
+            element4.click();
+        }
+        */
         // 檢查是否有登出按鈕
-        WebElement element = driver.findElement(By.xpath("//div[text()='歡迎回來！']"));
-        if (element.isDisplayed()) {
-            assert true;
-        } else {
-            assert false;
+        while (true) {
+            try {
+                WebElement element = driver.findElement(By.xpath("//a[text()='登出']"));
+                if (element.isDisplayed()) break;
+            } catch (Exception e) {
+                TimeUnit.SECONDS.sleep(1);
+            }
         }
         driver.quit();
     }
